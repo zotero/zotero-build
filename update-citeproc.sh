fba353dfa8cd84a3182fdeba5d84f5f61da7abac
Aurimas Vinckevicius <aurimas.dev@gmail.com>
2015-07-21 13:54:07 -0500

    Add script to update citeproc directly from bitbucket

---
+++ update-citeproc.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+ZOTEROSRC="../zotero"
+if [ -e "./config.sh" ]; then
+	. ./config.sh
+fi
+
+outFile="$ZOTEROSRC/chrome/content/zotero/xpcom/citeproc.js"
+
+if [ ! -e "$outFile" ]; then
+	>&2 echo "$outFile not found. Looking for Zotero source in $(readlink -f $ZOTEROSRC)"
+	exit 78 # EX_CONFIG: configuration error (from sysexits.h)
+fi
+
+echo "/*" > "$outFile"
+wget -O - https://bitbucket.org/fbennett/citeproc-js/raw/default/LICENSE | sed -e 's/^/ * /' >> "$outFile"
+echo -e " */\n" >> "$outFile"
+wget -O - https://bitbucket.org/fbennett/citeproc-js/raw/default/citeproc.js >> "$outFile"
