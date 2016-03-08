04b72fa5abfc62f5712082687dee30bcb345fe31
Dan Stillman <dstillman@zotero.org>
2016-03-07 19:12:29 -0500

    Get citeproc-js from new location on GitHub

--- update-citeproc.sh
+++ update-citeproc.sh
@@ -12,6 +12,6 @@ if [ ! -e "$outFile" ]; then
 fi
 
 echo "/*" > "$outFile"
-curl https://bitbucket.org/fbennett/citeproc-js/raw/default/LICENSE | sed -e $'s/\r$//' | sed -e 's/^/ * /' >> "$outFile"
+curl https://raw.githubusercontent.com/Juris-M/citeproc-js/master/LICENSE | sed -e $'s/\r$//' | sed -e 's/^/ * /' >> "$outFile"
 echo " */" >> "$outFile"
-curl https://bitbucket.org/fbennett/citeproc-js/raw/default/citeproc.js >> "$outFile"
+curl https://raw.githubusercontent.com/Juris-M/citeproc-js/master/citeproc.js >> "$outFile"
